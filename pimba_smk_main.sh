a5009ff4a6005e2938db9423c5cb38ebfba7c6cf
tiagolbiotech <tiagolbiotech@gmail.com>
2024-11-11 14:51:45 -0300

    adding singularity-args to the bash script

--- pimba_smk_main.sh
+++ pimba_smk_main.sh
@@ -44,41 +44,64 @@ if [[ "$prepare_mode" != "paired_end" && "$prepare_mode" != "single_index" && "$
     show_help
 fi
 
-# Check PIMBA run mode
-if [ -z "$run_mode" ]; then
-    echo "PIMBA run mode (database name) must be specified"
-    show_help
-fi
-
 # Check PIMBA plot mode
 if [[ "$plot_mode" != "yes" && "$plot_mode" != "no" ]]; then
     echo "Invalid PIMBA plot mode: $plot_mode"
     show_help
 fi
 
+# Path to the config file
+config_file="config/config.yaml"
+
+# Extract the path for rawdatadir from the config file
+rawdatadir=$(awk '/rawdatadir:/ {print $2}' "$config_file")
+
+# Extract the path for raw_fastq_single from the config file
+raw_fastq_single=$(grep '^raw_fastq_single:' "$config_file" | awk '{print $2}')
+
+# Extract the path for raw_fastq_dual from the config file
+raw_fastq_dual=$(grep '^raw_fastq_dual:' "$config_file" | awk '{print $2}')
+
+# Extract the path for NCBI-DB from the config file
+ncbi_db=$(grep '^NCBI-DB:' "$config_file" | awk '{print $2}')
+
+
 # Implement the actions based on the arguments
 if [ "$prepare_mode" == "paired_end" ]; then
     echo "Running PIMBA in paired_end prepare mode"
-    snakemake --snakefile workflow/Snakefile_prepare_paired --use-singularity --configfile config/config.yaml -j 8
+    snakemake --snakefile workflow/Snakefile_prepare_paired --use-singularity --configfile "$config_file" --cores 8 --singularity-args "-B $rawdatadir"
 elif [ "$prepare_mode" == "single_index" ]; then
     echo "Running PIMBA in single_index prepare mode"
-    snakemake --snakefile workflow/Snakefile_prepare_single_index --use-singularity --configfile config/config.yaml -j 8
+    raw_fastq_dir=$(dirname "$raw_fastq_single")
+    snakemake --snakefile workflow/Snakefile_prepare_single_index --use-singularity --configfile "$config_file" --cores 8 --singularity-args "-B $raw_fastq_dir"
 elif [ "$prepare_mode" == "dual_index" ]; then
     echo "Running PIMBA in dual_index prepare mode"
-    snakemake --snakefile workflow/Snakefile_prepare_dual_index --use-singularity --configfile config/config.yaml -j 8
+    raw_fastq_dir=$(dirname "$raw_fastq_dual")
+    snakemake --snakefile workflow/Snakefile_prepare_dual_index --use-singularity --configfile "$config_file" --cores 8 --singularity-args "-B $raw_fastq_dir"
 elif [ "$prepare_mode" == "no" ]; then
-    echo "No preparation mode selected"
+    echo "No prepare mode selected"
 fi
 
-if [ ! -z "$run_mode" ]; then
+# Check if $run_mode contains "NCBI" and adjust the snakemake command accordingly
+if [[ "$run_mode" == *"NCBI"* ]]; then
     echo "Running PIMBA with database: $run_mode"
-    snakemake --snakefile workflow/Snakefile_run --use-singularity --configfile config/config.yaml --cores 8 --singularity-args "-B /home/tfleao/Desktop/ITV/pimba_training/pimba/taxdump/:/taxdump"
+    snakemake --snakefile workflow/Snakefile_run --use-singularity --configfile "$config_file" --cores 8 --singularity-args "-B /home/tfleao/Desktop/ITV/pimba_training/pimba/taxdump/:/taxdump -B $ncbi_db"
+else
+    # Extract the path for the database corresponding to the run_mode
+    db_path=$(grep "^$run_mode-DB:" "$config_file" | awk '{print $2}')
+    if [ -z "$db_path" ]; then
+        echo "Database path for $run_mode-DB not found in config file."
+        db_path=$run_mode
+        exit 1
+    fi
+    echo "Running PIMBA with database: $run_mode"
+    snakemake --snakefile workflow/Snakefile_run --use-singularity --configfile "$config_file" --cores 8 --singularity-args "-B $db_path"
 fi
 
 if [ "$plot_mode" == "yes" ]; then
     echo "Plotting graphs as specified"
-    snakemake --snakefile workflow/Snakefile_plot --use-singularity --configfile config/config.yaml --cores 8
-elif [ "$plot_mode" == "no" ]; then
+    snakemake --snakefile workflow/Snakefile_plot --use-singularity --configfile "$config_file" --cores 8
+elif [ "$plot_mode" == "no" ];then
     echo "Skipping graph plotting as specified"
 fi
 
