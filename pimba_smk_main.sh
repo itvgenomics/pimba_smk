0b5ba46cae043c939f7c77169909c7147d01ef5a
tiagolbiotech <tiagolbiotech@gmail.com>
2025-04-01 10:20:58 -0300

    removing config from singularity args

--- pimba_smk_main.sh
+++ pimba_smk_main.sh
@@ -45,15 +45,15 @@ metadata=$(grep '^metadata:' "$config_file" | awk '{print $2}' | tr -d "'")
 if [ "$prepare_mode" != "no" ]; then
     if [ "$prepare_mode" == "paired_end" ]; then
         echo "Running PIMBA in paired_end prepare mode"
-        snakemake --snakefile workflow/Snakefile_prepare_paired --use-singularity --configfile "$config_file" --cores "$threads" --singularity-args "-B $rawdatadir -B $config_file"
+        snakemake --snakefile workflow/Snakefile_prepare_paired --use-singularity --configfile "$config_file" --cores "$threads" --singularity-args "-B $rawdatadir"
     elif [ "$prepare_mode" == "single_index" ]; then
         echo "Running PIMBA in single_index prepare mode"
         raw_fastq_dir=$(dirname "$raw_fastq_single")
-        snakemake --snakefile workflow/Snakefile_prepare_single_index --use-singularity --configfile "$config_file" --cores "$threads" --singularity-args "-B $raw_fastq_dir -B $config_file"
+        snakemake --snakefile workflow/Snakefile_prepare_single_index --use-singularity --configfile "$config_file" --cores "$threads" --singularity-args "-B $raw_fastq_dir"
     elif [ "$prepare_mode" == "dual_index" ]; then
         echo "Running PIMBA in dual_index prepare mode"
         raw_fastq_dir=$(dirname "$raw_fastq_dual")
-        snakemake --snakefile workflow/Snakefile_prepare_dual_index --use-singularity --configfile "$config_file" --cores "$threads" --singularity-args "-B $raw_fastq_dir -B $config_file"
+        snakemake --snakefile workflow/Snakefile_prepare_dual_index --use-singularity --configfile "$config_file" --cores "$threads" --singularity-args "-B $raw_fastq_dir"
     fi
 else
     echo "Skipping PIMBA prepare as specified"
@@ -63,9 +63,9 @@ if [ "$run_mode" != "no" ]; then
     if [[ "$run_mode" == *"NCBI"* ]]; then
         echo "Running PIMBA with database $run_mode plus remote mode as $remote"
         if [ "$remote" == "yes" ]; then
-            snakemake --snakefile workflow/Snakefile_run --use-singularity --configfile "$config_file" --cores "$threads" --singularity-args "-B $taxdump:/taxdump -B $taxdump -B $config_file"
+            snakemake --snakefile workflow/Snakefile_run --use-singularity --configfile "$config_file" --cores "$threads" --singularity-args "-B $taxdump:/taxdump -B $taxdump"
         else
-            snakemake --snakefile workflow/Snakefile_run --use-singularity --configfile "$config_file" --cores "$threads" --singularity-args "-B $ncbi_db -B $taxdump:/taxdump -B $taxdump -B $config_file"
+            snakemake --snakefile workflow/Snakefile_run --use-singularity --configfile "$config_file" --cores "$threads" --singularity-args "-B $ncbi_db -B $taxdump:/taxdump -B $taxdump"
         fi
     else
         db_path=$(grep "^$run_mode-DB:" "$config_file" | awk '{print $2}')
@@ -74,7 +74,7 @@ if [ "$run_mode" != "no" ]; then
             db_path=$run_mode
         fi
         echo "Running PIMBA with database: $run_mode"
-        snakemake --snakefile workflow/Snakefile_run --use-singularity --configfile "$config_file" --cores "$threads" --singularity-args "-B $db_path -B $config_file"
+        snakemake --snakefile workflow/Snakefile_run --use-singularity --configfile "$config_file" --cores "$threads" --singularity-args "-B $db_path"
     fi
 else
     echo "Skipping PIMBA run as specified"
@@ -82,7 +82,7 @@ fi
 
 if [ "$plot_mode" == "yes" ]; then
     echo "Plotting graphs as specified"
-    snakemake --snakefile workflow/Snakefile_plot --use-singularity --configfile "$config_file" --cores "$threads" --singularity-args "-B $metadata -B $config_file"
+    snakemake --snakefile workflow/Snakefile_plot --use-singularity --configfile "$config_file" --cores "$threads" --singularity-args "-B $metadata"
 else
     echo "Skipping graph plotting as specified"
 fi
